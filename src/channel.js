f35cdab2349d1c926edffef6af745ff0f6edb6be
Richard Bondi <r_bondi@hotmail.com>
2018-12-02 10:04:14 -0600

    commitment sequence

--- src/channel.js
+++ src/channel.js
@@ -1,5 +1,6 @@
 // ref: https://github.com/lightningnetwork/lightning-rfc/blob/master/03-transactions.md
 const Script = require('../src/script')
+const crypto = require('crypto')
 
 class Channel {
     constructor(local_funding_pubkey, remote_funding_pubkey) {
@@ -9,8 +10,10 @@ class Channel {
 
     fund(inputs, satoshis, change_out) {
         let tx = {
+            version: 2,
+            locktime: 542251326,
             inputs: inputs,
-            outputs: []
+            outputs: [],
         }
         let to_local_out = {
             value: satoshis,
@@ -18,7 +21,30 @@ class Channel {
             scriptPubKey: Script.scriptPubKey(Script.createFundingScript(this.local_funding_pubkey, this.remote_funding_pubkey))
         }
         tx.outputs.push(to_local_out)
+        tx.outputs.push(change_out)
         return tx
+    }
+
+    setBasepoints(basepoint_open, basepoint_accept) {
+        this.basepoint_open = basepoint_open
+        this.basepoint_accept = basepoint_accept
+        this.obscurator = this.commitmentObscurator(basepoint_open, basepoint_accept)
+    }
+
+    commitmentSequence(num) {
+        const obsucreCommitNum = parseInt(this.obscurator.slice(0, 3).toString('hex'), 16) ^ num
+        const commitBuff = Buffer.from(obsucreCommitNum.toString(16), 'hex')
+        const sequenceBuff = Buffer.concat([Buffer.from([0x80]), commitBuff])
+        return parseInt(sequenceBuff.toString('hex'), 16)
+    }
+
+    commitmentObscurator(basepoint_open, basepoint_accept) {
+        basepoint_open = typeof basepoint_open == 'string' ? Buffer.from(basepoint_open,'hex') : basepoint_open
+        basepoint_accept = typeof basepoint_accept == 'string' ? Buffer.from(basepoint_accept,'hex') : basepoint_accept
+        const basepoint = Buffer.concat([basepoint_open, basepoint_accept])
+        const hash = crypto.createHash('sha256').update(basepoint).digest()
+        const last48bits = hash.slice(-6)
+        return last48bits
     }
 
 }
--- test/channel.spec.js
+++ test/channel.spec.js
@@ -3,24 +3,32 @@ const Script = require('../src/script')
 const testdata = require('./testdata')
 const assert = require('assert');
 
-describe('Test transactions', function () {
+const commit = testdata.commitment
+const common = commit.common
+const txs = commit.txs
+const funding = testdata.funding
+
+describe('Test funding transactions', function () {
     it('properly creates funding wscript', function () {
-        const local = testdata.funding.local_funding_pubkey
-        const remote = testdata.funding.remote_funding_pubkey
+        const local = funding.local_funding_pubkey
+        const remote = funding.remote_funding_pubkey
         const witness = Script.createFundingScript(local, remote)
         const witstr = witness.toString('hex')
-        assert.strictEqual(witstr, testdata.funding.witness_script)
+        assert.strictEqual(witstr, funding.witness_script)
     })
 
     it('properly creates to_local wscript', function () {
-        const revpk = testdata.commitment.common.local_revocation_pubkey
-        const delpk = testdata.commitment.common.local_delayedpubkey
-        const to_self_delay = testdata.commitment.common.local_delay
+        const revpk = common.local_revocation_pubkey
+        const delpk = common.local_delayedpubkey
+        const to_self_delay = common.local_delay
         const witness = Script.toLocalOutput(revpk, delpk, to_self_delay)
         const witstr = witness.toString('hex')
         assert.strictEqual(witstr, testdata.commitment.txs[0].to_local.wscript)
     })
 
+})
+
+describe('Test commitment transactions', function () {
     it('properly creates to_local scriptPubKey', function () {
         const local = testdata.funding.local_funding_pubkey
         const remote = testdata.funding.remote_funding_pubkey
@@ -28,5 +36,14 @@ describe('Test transactions', function () {
         const intx = testdata.funding.input_tx
         const tx = chan.fund([{txid: intx.input_txid, n: intx.input_index}], intx.funding_satoshis, '')
         assert.strictEqual(tx.outputs[0].scriptPubKey.toString('hex'), "0020c015c4a6be010e21657068fc2e6a9d02b27ebe4d490a25846f7237f104d1a3cd")
+    })
+
+    it('properly creates sequence', function () {
+        const local = testdata.funding.local_funding_pubkey
+        const remote = testdata.funding.remote_funding_pubkey
+        const chan = new Channel(local, remote)
+        chan.setBasepoints(common.local_payment_basepoint, common.remote_payment_basepoint)
+        const sequence = chan.commitmentSequence(0)
+        assert.strictEqual(sequence, 2150346808)
     })
 })
